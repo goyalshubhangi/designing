dfd402c716a73db5c819b103f7b68f3320615eb6
Faisal Akhtar PC <faisalakhtar1999@gmail.com>
2020-06-02 22:28:40 +0530

    Web interface for illustrations

---
+++ assets/script.js
@@ -0,0 +1,105 @@
+function formatDate(date) {
+	date = new Date(date)
+	return new Intl.DateTimeFormat('en-US', { hour : 'numeric', minute : 'numeric', dayPeriod: 'short' }).format(date)
+	+ " on "
+	+ date.getDate() + "<sup>" + (date.getDate()>0?['th','st','nd','rd'][(date.getDate()>3 && date.getDate()<21) || date.getDate()%10>3?0:date.getDate()%10]:'')
+	+ "</sup> "
+	+ new Intl.DateTimeFormat('en-US', { month: 'short' }).format(date)
+	+ " ("
+	+ new Intl.DateTimeFormat('en-US', { weekday : 'long' }).format(date)
+	+ ")"
+}
+
+function init(json) {
+	visibleDiv()
+	var xhttp = new XMLHttpRequest()
+	xhttp.onload = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			let files = JSON.parse(this.responseText)
+
+			// Sorting descending on date
+			files.sort( function(a, b) {
+				return (new Date(b.date)) - (new Date(a.date))
+			})
+
+			addArticles(files)
+
+			// setTimeout(function () { document.getElementsByClassName("loading")[0].style.opacity = "0" }, 1000)
+			// setTimeout(function () { document.getElementsByClassName("loading")[0].remove() }, 2000)
+		} else {
+			showError(this.status, this.statusText)
+		}
+	}
+	xhttp.onerror = function() {
+		showError(this.status, this.statusText)
+	}
+	xhttp.open("GET", json, true)
+	xhttp.send()
+}
+
+function addArticles(files) {
+	let article, img, div
+	for (var i in files) {
+		article = document.createElement("article")
+		article.setAttribute("id","article"+files[i].id)
+		article.setAttribute("data-info",JSON.stringify({"auth":files[i].auth,"date":files[i].date}))
+
+		img = document.createElement("img")
+		img.setAttribute("src","SVGs/"+files[i].src)
+		img.setAttribute("onclick","openImg(this)")
+
+		a = document.createElement("a")
+		a.innerHTML = files[i].auth
+
+		article.append(img)
+		article.append(a)
+
+		document.getElementById("articles").append(article)
+	}
+}
+
+function showError(statusCode, statusText) {
+	document.getElementsByTagName("section")[0].style.opacity = "0"
+	setTimeout(function () {
+		let temp = "<main>\n\t<h1>ERROR " + statusCode + "</h1>\n\t<p>( "	+ statusText + " )</p>\n</main>"
+		document.getElementsByTagName("body")[0].innerHTML = temp
+	}, 1000)
+}
+
+window.onscroll = function() { visibleDiv() }
+
+function openImg(img) {
+	document.getElementsByTagName('body')[0].style.overflow = "hidden"
+	document.getElementsByClassName('maGallery')[0].style.display = "block"
+
+	let obj = JSON.parse(img.parentNode.getAttribute("data-info"))
+	document.querySelector(".date").innerHTML = formatDate(obj.date)
+	document.querySelector(".auth").innerHTML = obj.auth
+	document.querySelector(".image>img").src = img.src
+}
+
+isInViewport = (faisal,x)=>{
+	var bounding = faisal.getBoundingClientRect();
+	return(bounding.top<=100 && bounding.bottom>100)
+};
+
+function visibleDiv () {
+	let menuItem = document.querySelector("menu").children
+	menuItem[0].classList.remove("selected")
+	menuItem[1].classList.remove("selected")
+	menuItem[2].classList.remove("selected")
+	if(isInViewport( document.getElementById('home') )) {
+		menuItem[0].classList.add("selected")
+	}
+	else if(isInViewport( document.getElementById('illustrations') )) {
+		menuItem[1].classList.add("selected")
+	}
+	else if(isInViewport( document.getElementById('contact') )) {
+		menuItem[2].classList.add("selected")
+	}
+}
+
+function toggleMenu() {
+	document.querySelector('.smallMenuBars').classList.toggle('open')
+	document.querySelector('.smallMenu').classList.toggle('open')
+}
